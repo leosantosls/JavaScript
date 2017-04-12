3dc7994fc43ff756a88ef3978b0ed92c1a41b1dd
leosantosls <leonardosantos969890@gmail.com>
2017-04-12 14:46:42 -0300

    Add files via upload

---
+++ e.js
@@ -0,0 +1,13 @@
+function evento_entrar()
+{
+alert("Seja Bem Vindo(a) Mestre");
+}
+function evento_sair()
+{
+alert("Até a Proxima Visitar Mestre");
+}
+function evento_clique()
+{
+ window.document.getElementById('id2').style.color='red';
+}
+
